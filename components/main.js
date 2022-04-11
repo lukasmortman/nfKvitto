5e9fb8ddfdf69d4c38e8023a10cfb52591d16595
lukasmortman <lukasbestnu@gmail.com>
2022-04-11 13:03:52 +0200

    test

--- components/main.js
+++ components/main.js
@@ -2,7 +2,7 @@ import {useState} from "react";
 import logo from '../public/loggan.jpg'
 import styles from './main.module.css'
 import Image from 'next/image'
-import Head from 'next/head'
+
 
 export default function Main() {
     const [state, setState] = useState({
@@ -62,16 +62,6 @@ export default function Main() {
                     </button>
                 </form>
             </div>
-            <Head>
-                <html lang="SE"/>
-                <meta name="description" content="En hemsida för att hantera den Naturvetenskapliga föreningens kvitton och köp."/>
-                <title>NF Kvitton</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/site.webmanifest"/>
-            </Head>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Main from "../components/main";
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <Main/>
+        <>
+            <Head>
+                <html lang="SE"/>
+                <meta name="description" content="En hemsida för att hantera den Naturvetenskapliga föreningens kvitton och köp."/>
+                <title>NF Kvitton</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+            </Head>
+            <section>
+                <Main/>
+            </section>
+        </>
     )
 }
