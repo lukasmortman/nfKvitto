147e310130dc5f3d41b56e3b8adc56f2422522ad
lukasmortman <lukasbestnu@gmail.com>
2022-04-11 15:35:08 +0200

    ändrade css från grid till flex

--- components/main.js
+++ components/main.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import logo from '../public/loggan.jpg'
+import logo from '../public/NF logga.svg'
 import styles from './test.module.css'
 import Image from 'next/image'
 
