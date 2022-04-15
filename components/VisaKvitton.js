6d92320ab83c1dfaf5b2b2c3c49967b68ef07228
lukasmortman <lukasbestnu@gmail.com>
2022-04-15 17:37:08 +0200

    la till padding och tog bort konstigt tecken

--- components/VisaKvitton.js
+++ components/VisaKvitton.js
@@ -29,7 +29,7 @@ export default function VisaKvitton({data}) {
                         <p>swishnummer: {swish}</p>
                     </div>
                 </div>
-            ))};
+            ))}
         </div>
     )
 
