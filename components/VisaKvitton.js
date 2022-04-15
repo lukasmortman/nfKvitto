1e46ddad7df8f039ac62d3e24295e555cbae5c89
lukasmortman <lukasbestnu@gmail.com>
2022-04-15 18:52:33 +0200

    fixade lite css x2, så bra det kan blir nu tror jag

--- components/VisaKvitton.js
+++ components/VisaKvitton.js
@@ -17,19 +17,23 @@ export default function VisaKvitton({data}) {
                 />
                 <h1 className={styles.FlexAndCenter} style={{margin: "-0.5vh"}}>nf kvitto</h1>
             </div>
+            <div className={styles.Padding1REM}>
             {data.map(({vara, pris, datum, swish, bild}) => (
-                <div className={`${styles.FlexAndCenter} ${styles.Padding1REM}`}>
-                    <div className={styles.BILD}>
+                <div className={`${styles.parent}`}>
+                    <div className={styles.div5}>
                         <Image src={bild} alt={"bild på kvittot"} height={80} width={80}/>
                     </div>
-                    <div className={styles.ROWFLEX}>
-                        <p>namn på vara: {vara} </p>
-                        <p>pris på vara: {pris}</p>
-                        <p>datum köpt: {datum}</p>
-                        <p>swishnummer: {swish}</p>
+                    <div className={styles.div1}><p>namn på vara: {vara} </p>
+                    </div>
+                    <div className={styles.div2}><p>pris på vara: {pris}</p>
+                    </div>
+                    <div className={styles.div3}><p>datum köpt: {datum}</p>
+                    </div>
+                    <div className={styles.div4}><p>swishnummer: {swish}</p>
                     </div>
                 </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 
