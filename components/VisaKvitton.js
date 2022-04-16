63d5575cc33c8ce7eadaf21ee96215d71a828632
lukasmortman <lukasbestnu@gmail.com>
2022-04-16 20:30:47 +0200

    snodde bättre excel kod från internet :)

--- components/VisaKvitton.js
+++ components/VisaKvitton.js
@@ -1,15 +1,39 @@
 import Image from 'next/image'
 import logo from "../public/loggan.jpg";
 import styles from '../styles/VisaKvitto.module.css'
-import exportFromJSON from 'export-from-json'
+import { saveAs } from "file-saver";
+import XlsxPopulate from "xlsx-populate";
 
 export default function VisaKvitton({data}) {
-    const exportType = "xls"
-    const fileName = 'kvittonNF'
-    const fields = ["vara","pris","bild","datum","swish"]
-    function ExportToExcel() {
-        exportFromJSON({ data, fileName, exportType, fields })
+    function getSheetData(data, header) {
+        const fields = Object.keys(data[0]);
+        const sheetData = data.map(function (row) {
+            return fields.map(function (fieldName) {
+                return row[fieldName] ? row[fieldName] : "";
+            });
+        });
+        sheetData.unshift(header);
+        return sheetData;
     }
+    async function saveAsExcel() {
+        let header = ["id","vara","pris","datum","bild","swish"]
+        XlsxPopulate.fromBlankAsync().then(async (workbook) => {
+            const sheet1 = workbook.sheet(0);
+            const sheetData = getSheetData(data, header);
+            const totalColumns = sheetData[0].length;
+
+            sheet1.cell("A1").value(sheetData);
+            const range = sheet1.usedRange();
+            const endColumn = String.fromCharCode(64 + totalColumns);
+            sheet1.row(1).style("bold", true);
+            sheet1.range("A1:" + endColumn + "1").style("fill", "BFBFBF");
+            range.style("border", true);
+            return workbook.outputAsync().then((res) => {
+                saveAs(res, "KvittonNF.xlsx");
+            });
+        });
+    }
+
 
     return (
         <div className={styles.container}>
@@ -25,7 +49,7 @@ export default function VisaKvitton({data}) {
                 />
                 <h1 className={styles.FlexAndCenter} style={{margin: "-0.5vh"}}>nf kvitto</h1>
                 <span className={styles.FlexAndCenter}>
-                    <button className={styles.FlexAndCenter} onClick={()=>ExportToExcel()}>
+                    <button className={styles.FlexAndCenter} onClick={()=>saveAsExcel()}>
                         exportera till excel
                     </button>
                 </span>
