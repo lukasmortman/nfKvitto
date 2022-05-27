aaf43b6bc1259f04e02c06c86305b805445cd202
lukasmortman <lukasbestnu@gmail.com>
2022-05-27 15:45:15 +0200

    testar lite med avgifter och intäkter

--- components/SkickaKvitto.js
+++ components/SkickaKvitto.js
@@ -4,7 +4,6 @@ import {initializeApp} from "firebase/app";
 import {getDownloadURL, getStorage, ref, uploadString} from "firebase/storage";
 
 
-
 export default function SkickaKvitto() {
     const [state, setState] = useState({
         vara: "",
@@ -13,6 +12,7 @@ export default function SkickaKvitto() {
         bild: "",
         swish: "",
         kategori: "",
+        typavkop: "",
     })
     const [skickat, setSkickat] = useState("")
     const [base64, setBase64] = useState("")
@@ -20,7 +20,14 @@ export default function SkickaKvitto() {
 
     const handleSubmit = event => {
         event.preventDefault();
-        postData({vara:state.vara,pris:state.pris,kategori: state.kategori,datum:state.datum,bild:base64,swish:state.swish})
+        test(JSON.stringify({
+            vara: state.vara,
+            pris: state.pris,
+            kategori: state.kategori,
+            datum: state.datum,
+            bild: base64,
+            swish: state.swish
+        }))
         setState({
             vara: "",
             pris: "",
@@ -43,8 +50,8 @@ export default function SkickaKvitto() {
         if (event.target.name === "bild") {
             const file = event.target.files[0];
             const reader = new FileReader();
-            reader.onloadend =  function () {
-                 setBase64(reader.result)
+            reader.onloadend = function () {
+                setBase64(reader.result)
             }
             reader.readAsDataURL(file);
             const value = event.target.value;
@@ -62,7 +69,7 @@ export default function SkickaKvitto() {
     }
 
 
-    function test(test){
+    function test(test) {
         const firebaseConfig = {
             apiKey: process.env.apiKey,
             authDomain: process.env.authDomain,
@@ -81,15 +88,42 @@ export default function SkickaKvitto() {
             return getDownloadURL(snapshot.ref)
         }).then(async downloadURL => {
             try {
-                if(state.kategori===""){
+                if (state.kategori === "" && state.typavkop === "intäkt") {
                     await fetch('/api/SkickaData', {
                         method: 'POST',
-                        body: JSON.stringify({vara:state.vara,pris:Math.round(state.pris),kategori: "Medlemsavgifter",datum:state.datum,bild:downloadURL,swish:state.swish}),
+                        body: JSON.stringify({
+                            vara: state.vara,
+                            pris: Math.round(state.pris),
+                            kategori: "Medlemsavgifter",
+                            datum: state.datum,
+                            bild: downloadURL,
+                            swish: state.swish
+                        }),
                     });
-                }else{
+                } else if(state.kategori === "" && state.typavkop === "avgift"){
                     await fetch('/api/SkickaData', {
                         method: 'POST',
-                        body: JSON.stringify({vara:state.vara,pris:Math.round(state.pris),kategori: state.kategori,datum:state.datum,bild:downloadURL,swish:state.swish}),
+                        body: JSON.stringify({
+                            vara: state.vara,
+                            pris: Math.round(state.pris),
+                            kategori: "Laborationer",
+                            datum: state.datum,
+                            bild: downloadURL,
+                            swish: state.swish
+                        }),
+                    });
+                }
+                else {
+                    await fetch('/api/SkickaData', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            vara: state.vara,
+                            pris: Math.round(state.pris),
+                            kategori: state.kategori,
+                            datum: state.datum,
+                            bild: downloadURL,
+                            swish: state.swish
+                        }),
                     });
                 }
             } catch (error) {
@@ -98,17 +132,33 @@ export default function SkickaKvitto() {
         })
     }
 
-    const postData = async (form) => {
-        test(JSON.stringify(form))
-    }
 
     // TODO: fixa så att det inte står vara något mer kom på något annat
     // TODO: lös också så att avgift/intäkt funktion finns, typ så att man bestämmer det innan eller något
-    return (
+    return state.typavkop === "avgift"
+        ? (
             <div className={styles.Form}>
+                <div className={styles.centerKnappar}>
+                    <button name="typavkop" onClick={() => setState({...state, "typavkop": "avgift"
+                    })}>avgifter
+                    </button>
+                    <button name="typavkop" onClick={() => setState({...state, "typavkop": "intäkt"
+                    })}>intäkt
+                    </button>
+                </div>
                 <form className={styles.formStyle} onSubmit={handleSubmit}>
+                    <label className={styles.labelStyle} htmlFor="kategori">kategori på avgift:</label>
+                    <select className={styles.kategori} name="kategori" id="kategori" defaultValue={state.kategori}
+                            required onChange={handleChange}>
+                        <option value="Laborationer">Laborationer</option>
+                        <option value="Kök&fester">Kök & fester</option>
+                        <option value="Försäljning">Försäljning</option>
+                        <option value="NF-artiklar">NF-artiklar</option>
+                        <option value="Övrigt">Övrigt</option>
+                    </select>
                     <label className={styles.labelStyle} htmlFor="vara">vara:</label>
-                    <input type="text" name="vara" placeholder="namn på vara (max 16 tecken)" value={state.vara} maxLength={16} required
+                    <input type="text" name="vara" placeholder="namn på vara (max 16 tecken)" value={state.vara}
+                           maxLength={16} required
                            onChange={handleChange}/>
                     <label className={styles.labelStyle} htmlFor="pris">pris:</label>
                     <input type="number" name="pris" placeholder="pris (skriv inte kr)" value={state.pris} required
@@ -116,26 +166,85 @@ export default function SkickaKvitto() {
                     <label className={styles.labelStyle} htmlFor="datum">datum:</label>
                     <input type="date" name="datum" value={state.datum} placeholder={Date.now()} required
                            onChange={handleChange}/>
-                    <label className={styles.labelStyle} htmlFor="kategori">kategori på köp:</label>
-                    <select className={styles.kategori} name="kategori" id="kategori" required onChange={handleChange}>
+                    <label className={styles.labelStyle} htmlFor="bild">kvitto:</label>
+                    <input type="file" accept="image/*" name="bild" value={state.bild} style={{alignSelf: "center"}}
+                           placeholder="bild på kvitto"
+                           required
+                           onChange={handleChange}/>
+                    <label className={styles.labelStyle} htmlFor="vara">swish-nummer:</label>
+                    <input type="tel" name="swish" value={state.swish} placeholder={"swishnummer"} required
+                           pattern="[0-9]{3}-[0-9]{7}|[0-9]{10}" onChange={handleChange}/>
+                    <button className={styles.buttonStyle} type="submit">
+                        skicka in kvitto
+                    </button>
+                    <p style={{
+                        display: "inline-block",
+                        marginLeft: "0.5vw",
+                        fontWeight: "bold",
+                        fontSize: "0.7rem"
+                    }}>{skickat}</p>
+                </form>
+            </div>
+        ) : state.typavkop === "intäkt" ?
+        (
+            <div className={styles.Form}>
+                <div className={styles.centerKnappar}>
+                    <button name="typavkop" onClick={() => setState({...state, "typavkop": "avgift"
+                    })}>avgifter
+                    </button>
+                    <button name="typavkop" onClick={() => setState({...state, "typavkop": "intäkt"
+                    })}>intäkt
+                    </button>
+                </div>
+                <form className={styles.formStyle} onSubmit={handleSubmit}>
+                    <label className={styles.labelStyle} htmlFor="kategori">kategori på intäkt:</label>
+                    <select className={styles.kategori} name="kategori" id="kategori" defaultValue={state.kategori}
+                            required onChange={handleChange}>
                         <option value="Medlemsavgifter">Medlemsavgifter</option>
-                        <option value="Laborationer">Laborationer</option>
                         <option value="Kök&fester">Kök & fester</option>
                         <option value="Försäljning">Försäljning</option>
                         <option value="NF-artiklar">NF-artiklar</option>
                         <option value="Övrigt">Övrigt</option>
                     </select>
+                    <label className={styles.labelStyle} htmlFor="vara">vara:</label>
+                    <input type="text" name="vara" placeholder="namn på vara (max 16 tecken)" value={state.vara}
+                           maxLength={16} required
+                           onChange={handleChange}/>
+                    <label className={styles.labelStyle} htmlFor="pris">pris:</label>
+                    <input type="number" name="pris" placeholder="pris (skriv inte kr)" value={state.pris} required
+                           onChange={handleChange}/>
+                    <label className={styles.labelStyle} htmlFor="datum">datum:</label>
+                    <input type="date" name="datum" value={state.datum} placeholder={Date.now()} required
+                           onChange={handleChange}/>
                     <label className={styles.labelStyle} htmlFor="bild">kvitto:</label>
-                    <input type="file" accept="image/*" name="bild" value={state.bild} placeholder="bild på kvitto" required
+                    <input type="file" accept="image/*" name="bild" value={state.bild} style={{alignSelf: "center"}}
+                           placeholder="bild på kvitto"
+                           required
                            onChange={handleChange}/>
                     <label className={styles.labelStyle} htmlFor="vara">swish-nummer:</label>
                     <input type="tel" name="swish" value={state.swish} placeholder={"swishnummer"} required
                            pattern="[0-9]{3}-[0-9]{7}|[0-9]{10}" onChange={handleChange}/>
-                        <button className={styles.buttonStyle} type="submit">
-                            skicka in kvitto
-                        </button>
-                    <p style={{display: "inline-block", marginLeft: "0.5vw", fontWeight: "bold", fontSize:"0.7rem"}}>{skickat}</p>
+                    <button className={styles.buttonStyle} type="submit">
+                        skicka in kvitto
+                    </button>
+                    <p style={{
+                        display: "inline-block",
+                        marginLeft: "0.5vw",
+                        fontWeight: "bold",
+                        fontSize: "0.7rem"
+                    }}>{skickat}</p>
                 </form>
             </div>
-    )
+        ) : (
+                <div className={styles.Form}>
+                    <div className={styles.centerKnappar}>
+                        <button name="typavkop" onClick={() => setState({...state, "typavkop": "avgift"
+                        })}>avgifter
+                        </button>
+                        <button name="typavkop" onClick={() => setState({...state, "typavkop": "intäkt"
+                        })}>intäkt
+                        </button>
+                    </div>
+                </div>
+            )
 }
