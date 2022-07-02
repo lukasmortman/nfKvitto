2fe815090c00733afabfdf222ecefc03686ef191
lukasmortman <lukasbestnu@gmail.com>
2022-07-02 13:27:39 +0200

    fallback verkar inte fungera

--- pages/admin/[vara]/[swish]/[pris].js
+++ pages/admin/[vara]/[swish]/[pris].js
@@ -56,7 +56,7 @@ export async function getStaticPaths() {
                 vara: data.vara, swish: data.swish,  pris: data.pris.toString()
             }
     }))
-    return { paths};
+    return { paths, fallback: false};
 }
 
 
